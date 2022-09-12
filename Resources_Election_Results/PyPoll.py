73399ecf9f613b4a7e87cc4c780a82a8d6d132bb
Amber Bethel <amberbethel@Ambers-MacBook-Air.local>
2022-09-12 14:52:18 -0400

    Adding the election analysis.

--- Resources_Election_Results/PyPoll.py
+++ Resources_Election_Results/PyPoll.py
@@ -44,49 +44,58 @@ with open(file_to_load) as election_data:
         #Add a vote to that candidate' count
         candidate_votes[candidate_name] += 1
     
-    # Determine the percentage of votes for each candidate by looping through the counts.
-    # 1. Iterate through the candidate list.
-    for candidate_name in candidate_votes:
-        #2. Retrieve vote count of a candidate.
-        votes = candidate_votes[candidate_name]
-        # Caldulate the percentage of votes
-        vote_percentage = float(votes) / float(total_votes) * 100
-        #Print the candidate name and percentage of votes.
-        print(f"{candidate_name}: recieved {vote_percentage}% of the vote.")
-    
-        #Determine the winning candidate by the number and percentage of votes
-        if (votes > winning_count) and (vote_percentage > winning_percentage):
-            # 2. If true then set winning_count = votes and winning_percent =
-            # vote_percentage.
-            winning_count = votes
-            winning_percentage = vote_percentage 
-            #3. Set the winning_candidate equal to the candidate's name
-            winning_candidate = candidate_name
+    #Save the results to out text file.
+    with open(file_to_save, "w") as txt_file:
+        # Print the final vote count to the terminal.
+        election_results = (
+            f"\nElection Results\n"
+            f"-------------------------\n"
+            f"Total Votes: {total_votes:,}\n"
+            f"-------------------------\n")
+        print(election_results, end="")
+        # Save the final vote count to the text file.
+        txt_file.write(election_results)
 
-    winning_candidate_summary = (
-        f"-------------------------\n"
-        f"Winner: {winning_candidate}\n"
-        f"Winning Vote Count: {winning_count:,}\n"
-        f"Winning Percentage: {winning_percentage:.1f}%\n"
-        f"-------------------------\n")
 
-print(winning_candidate_summary)
+        # Determine the percentage of votes for each candidate by looping through the counts.
+        # 1. Iterate through the candidate list.
+        for candidate_name in candidate_votes:
+            #2. Retrieve vote count of a candidate.
+            votes = candidate_votes[candidate_name]
+            # Caldulate the percentage of votes
+            vote_percentage = float(votes) / float(total_votes) * 100
+            #Print the candidate name and percentage of votes.
+            candidate_results = (f"{candidate_name}: recieved {vote_percentage}% of the vote.")
+            
+            print(candidate_results)
+            txt_file.write(candidate_results)
+            #Determine the winning candidate by the number and percentage of votes
+            if (votes > winning_count) and (vote_percentage > winning_percentage):
+                # 2. If true then set winning_count = votes and winning_percent =
+                # vote_percentage.
+                winning_count = votes
+                winning_percentage = vote_percentage 
+                #3. Set the winning_candidate equal to the candidate's name
+                winning_candidate = candidate_name
+
+        winning_candidate_summary = (
+            f"-------------------------\n"
+            f"Winner: {winning_candidate}\n"
+            f"Winning Vote Count: {winning_count:,}\n"
+            f"Winning Percentage: {winning_percentage:.1f}%\n"
+            f"-------------------------\n")
+
+#print(winning_candidate_summary)
 
 #Print the total votes
-print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+#candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+print(winning_candidate_summary)
+txt_file.write(winning_candidate_summary)
 
 
 
 
 
-#1. The total number of votes cast
-#2. A complete list of candidates who recieved votes
-#3. the percentage of votes each candidate won.
-#4. The total number of votes each candidate won.
-#5. The winner of the election based on popular vote.
-
-
-#Choose the file
 
 
 
